ab7ffb7d4e62195a9a9ba88c7f215de9c81fcd4a
Satgawas007 <48801433+Satgawas007@users.noreply.github.com>
2019-04-02 16:31:56 +0530

    ButtonAndCheckboxProject

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {CheckboxDemo} from './CheckboxDemo';
+import {ButtonDemo} from './ButtonDemo';
+
+ReactDOM.render(
+  <ButtonDemo />,
+  document.getElementById('root')
+);
